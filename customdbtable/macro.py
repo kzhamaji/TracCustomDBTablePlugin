92a80f5b4183d18ed608d8faa2e644c44446ada0
Masakazu HAMAJI <kzhamaji@gmail.com>
2016-03-30 11:44:22 +0900

    Add CustomDBTableFlat macro.

--- customdbtable/macro.py
+++ customdbtable/macro.py
@@ -12,7 +12,6 @@ from customdbtable.api import CustomDBTableSystem
 
 class CustomDBTableMacro(WikiMacroBase):
     """A macro to display a table fo CustumDBTable
-    All arguments are optional:
     {{{
     [[CustomDBTable(<table_name>[,<options>]]]
     }}}
@@ -54,4 +53,47 @@ class CustomDBTableMacro(WikiMacroBase):
             for c in _columns:
                 tr(tag.td(row[c]))
             ttable(tr)
+        return ttable
+
+
+class CustomDBTableFlatMacro(WikiMacroBase):
+    """A macro to display a simple table of CustumDBTable 'name' field.
+    {{{
+    [[CustomDBTable(<table_name>[,<options>]]]
+    }}}
+
+    Available options:
+     * `exclude=` - `|` exclude item by 'name' column
+     * `column_count=` - The number of column count. (Default: 3)
+    """
+    def expand_macro(self, formatter, name, content):
+        args, opts = parse_args(content)
+        if len(args) != 1:
+            raise TracError("Requied single table name")
+        table = args[0]
+        excludes = [e.strip() for e in opts.get('exclude', '').split('|')]
+        ccount = int(opts.get('column_count', 3))
+
+        cdtsys = CustomDBTableSystem(self.env)
+        try:
+            items = cdtsys.sorted_column(table, 'name')
+        except:
+            raise TracError("Table not found: %s" % table)
+
+        if excludes:
+            items = [e for e in items if e not in excludes]
+
+        ttable = tag.table(class_='wiki customdbtable')
+        tr = td = None
+        i = 0
+        for item in items:
+            if i % ccount == 0:
+                tr = tag.tr()
+                ttable(tr)
+            td = tag.td(item)
+            tr(td)
+            i += 1
+        while i % ccount != 0:
+            tr(tag.td())
+            i += 1
         return ttable
